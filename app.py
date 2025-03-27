ad3ad121d224e80d5376ba2c6486e7a487eacb44
scruffyscarf <114242238+scruffyscarf@users.noreply.github.com>
2025-03-27 21:46:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello, Docker + GitHub Actions!"
+    return "Hello, Docker CI/CD!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
